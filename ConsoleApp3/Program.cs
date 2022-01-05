cda3ea21e08cb32dc70f20ee52867f9e7585a087
PC <PC@DESKTOP-U995AMT>
2022-01-05 17:41:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+
+		string my1Name;
+		my1Name = "Jane";
+
+		Console.WriteLine(my1Name);
+
+		Console.ReadKey();
+	}
+}
